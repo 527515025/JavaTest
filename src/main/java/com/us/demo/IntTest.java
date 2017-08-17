7638260a5123a24bdd6649894792847351bb8d13
杨伊博 <527515025@qq.com>
2017-08-17 18:08:04 +0800

    add note

--- src/main/java/com/us/demo/IntTest.java
+++ src/main/java/com/us/demo/IntTest.java
@@ -5,17 +5,32 @@ package com.us.demo;
  */
 public class IntTest {
     public static void main(String[] args) {
-        compare();
+        compareInteger();
+        compareInt();
     }
 
-    public static void compare() {
-        Integer a = 2015;
-        Integer b = 2015;
+    /**
+     * Integer 型值 == 比较最大＋－127 Integer.high是128
+     * Integer 在-128<=i<=127的时候是直接用的int原始数据类型，而超出了这个范围则是new了一个Integer对象。
+     * 我们知道"=="符号在比较对象的时候是比较的内存地址，而对于原始数据类型是直接比对的数据值。
+     * equals 为对象比较
+     */
+    public static void compareInteger() {
+        Integer a = 128;
+        Integer b = 128;
         if (a == b) {
-            System.out.println("a == b: true");
+            System.out.println("compareInteger: a == b: true");
         }
         if (a.equals(b)) {
-            System.out.println("a.equals(b) : true");
+            System.out.println("compareInteger: a.equals(b) : true");
+        }
+    }
+
+    public static void compareInt() {
+        int a = 2015;
+        int b = 2015;
+        if (a == b) {
+            System.out.println("compareInt: a == b: true");
         }
     }
 }
