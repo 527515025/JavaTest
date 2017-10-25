77125d7a94b5be548e1e0ecbd74e3f354fa82e66
杨伊博 <527515025@qq.com>
2017-10-25 23:22:42 +0800

    add http and https

--- src/main/java/com/us/IO/FileTest.java
+++ src/main/java/com/us/IO/FileTest.java
@@ -10,6 +10,7 @@ import java.io.RandomAccessFile;
 public class FileTest {
 
     private static String OS_NAME = System.getProperty("os.name").toLowerCase();
+    private static Long pos = 0L;
 
     public static void main(String[] args) {
         System.out.println(OS_NAME.contains("mac"));
@@ -25,19 +26,18 @@ public class FileTest {
     /**
      * 获取当前路径下的所有文件
      */
-    private static void getAllFileInPath(){
+    private static void getAllFileInPath() {
         File file = new File("src/main/resource");
         System.out.println(file);
     }
 
     /**
-     *
      * @throws IOException
      */
     private static void write() throws IOException {
         RandomAccessFile file = new RandomAccessFile("src/main/resource/testFile.txt", "rw");
-        file.seek(file.length());
-        file.write("a bcd你好啊的撒法\n".getBytes("UTF-8"));
+        file.seek(pos);
+        file.write("a bcd你好啊的撒法".getBytes("UTF-8"));
         file.close();
         System.out.println();
 
@@ -45,7 +45,8 @@ public class FileTest {
 
     private static void read() throws IOException {
         RandomAccessFile rf = new RandomAccessFile("src/main/resource/testFile.txt", "r");
-        System.out.println(new String(rf.readLine().getBytes("ISO-8859-1"),"utf-8"));
+        System.out.println(new String(rf.readLine().getBytes("ISO-8859-1"), "utf-8"));
+        pos = rf.length();
         rf.close();
         System.out.println();
 
