c8a41bd1a67f4d7d1769d52d47e7bb0be402f302
杨伊博 <527515025@qq.com>
2019-05-05 15:23:45 +0800

    modify timing

--- src/main/java/com/us/python/timing.py
+++ src/main/java/com/us/python/timing.py
@@ -1,4 +1,4 @@
-
+# python 的定时任务
 import schedule
 import time
  
@@ -14,3 +14,23 @@ schedule.every().day.at("16:06").do(job)
  
 while True:
     schedule.run_pending()
+
+
+
+# python 的定时任务系统
+# import datetime
+#
+# sched_Timer=datetime.datetime(2019,4,1,16,29,0);
+# print(sched_Timer)
+# flag = 0;
+# while True:
+#     now = datetime.datetime.now();
+#     if now==sched_Timer:
+#         print('run Task')
+#         flag=1
+#     else:
+#         if flag==1:
+#             sched_Timer=sched_Timer+datetime.timedelta(minutes=1);
+#             flag=0
+#
+#
