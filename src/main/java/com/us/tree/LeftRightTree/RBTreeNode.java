dbe595080f20073269fe6dd13edddc2da9f32242
杨伊博 <527515025@qq.com>
2019-10-28 15:04:26 +0800

    大的 RBTRee

---
+++ src/main/java/com/us/tree/LeftRightTree/RBTreeNode.java
@@ -0,0 +1,90 @@
+package com.us.tree.LeftRightTree;
+
+/**
+ * 红黑树数据对象
+ * @author yyb
+ * @time 2019/10/28
+ */
+public class RBTreeNode<T extends Comparable<T>> {
+    private T value;
+    private RBTreeNode<T> left;
+    private RBTreeNode<T> right;
+    private RBTreeNode<T> parent;
+    private boolean red;
+
+    public RBTreeNode() {
+    }
+
+    public RBTreeNode(T value) {
+        this.value = value;
+    }
+
+    public RBTreeNode(T value, boolean isRed) {
+        this.value = value;
+        this.red = isRed;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    void setValue(T value) {
+        this.value = value;
+    }
+
+    RBTreeNode<T> getLeft() {
+        return left;
+    }
+
+    void setLeft(RBTreeNode<T> left) {
+        this.left = left;
+    }
+
+    RBTreeNode<T> getRight() {
+        return right;
+    }
+
+    void setRight(RBTreeNode<T> right) {
+        this.right = right;
+    }
+
+    RBTreeNode<T> getParent() {
+        return parent;
+    }
+
+    void setParent(RBTreeNode<T> parent) {
+        this.parent = parent;
+    }
+
+    boolean isRed() {
+        return red;
+    }
+
+    boolean isBlack() {
+        return !red;
+    }
+
+    /**
+     * is leaf node
+     **/
+    boolean isLeaf() {
+        return left == null && right == null;
+    }
+
+    void setRed(boolean red) {
+        this.red = red;
+    }
+
+    void makeRed() {
+        red = true;
+    }
+
+    void makeBlack() {
+        red = false;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
