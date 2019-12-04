3fdc6f572c6890240bee8a8172dc51666095f039
杨伊博 <527515025@qq.com>
2019-12-04 17:19:21 +0800

    add method

--- src/main/java/com/us/basics/http/Test.java
+++ src/main/java/com/us/basics/http/Test.java
@@ -2,6 +2,7 @@ package com.us.basics.http;
 
 import com.alibaba.fastjson.JSON;
 import com.us.basics.http.logisticsMode.LogisticsResponse;
+import com.us.basics.http.logisticsMode.Result;
 import org.apache.http.HttpResponse;
 
 import java.util.HashMap;
@@ -16,13 +17,13 @@ public class Test {
         String host = "https://jisukdcx.market.alicloudapi.com";
         String path = "/express/query";
         String method = "GET";
-        String appcode = "xxxxxxxx";
+        String appcode = "xxx";
         Map<String, String> headers = new HashMap<>();
         //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
         headers.put("Authorization", "APPCODE " + appcode);
         Map<String, String> querys = new HashMap<String, String>();
         querys.put("number", "1193020101430");
-        querys.put("type", "auto");
+        querys.put("type", "yangyibo");
         try {
             /**
              * 重要提示如下:
@@ -34,13 +35,25 @@ public class Test {
              * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
              */
             HttpResponse response = HttpUtils.doGet(host, path, method, headers, querys);
-            if (response.getStatusLine().getStatusCode() == 200) {
-//                String resp = EntityUtils.toString(response.getEntity(), "UTF-8");
-
-                LogisticsResponse logisticsResponse = JSON.toJavaObject(HttpUtils.getJson(response), LogisticsResponse.class);
-                System.out.println(logisticsResponse.getMsg());
-            } else {
-                System.out.println("请求失败");
+            switch (response.getStatusLine().getStatusCode()) {
+                case 200:
+                    Result logisticsResponse = JSON.toJavaObject((JSON) HttpUtils.getJson(response).get("result"), Result.class);
+                    System.out.println(logisticsResponse.getNumber());
+                    break;
+                case 208:
+                    System.out.println("单号没有信息");
+                    break;
+                case 207:
+                    System.out.println("快递单号错误次数过多");
+                    break;
+                case 203:
+                    System.out.println("快递公司不存在");
+                    break;
+                case 205:
+                    System.out.println("没有信息");
+                    break;
+                default:
+                    System.out.println("没有查询到信息");
             }
         } catch (Exception e) {
             e.printStackTrace();
