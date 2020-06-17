10fb70dcd410cc5a3885c57f70c88c1ddbcef0bb
杨伊博 <527515025@qq.com>
2020-06-17 17:27:19 +0800

    两数之和

---
+++ src/main/java/com/us/acm/TwoSum.java
@@ -0,0 +1,64 @@
+package com.us.acm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ *
+ * @author yyb
+ * @time 2020/6/17
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] result = twoSum2(new int[]{1, 15, 3, 4, 5, 6, 7, 2, 9, 0}, 19);
+        Arrays.stream(result).parallel().forEach(x -> System.out.println(x));
+    }
+
+    /**
+     * 两次遍历
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer value = map.get(target - nums[i]);
+            if (null != value) {
+                return new int[]{i, value};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 一次遍历
+     * 在进行迭代并将元素插入到表中的同时，会回过头来检查表中是否已经存在当前元素所对应的目标元素。
+     * 如果它存在，那我们已经找到了对应解，并立即将其返回
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+            Integer value = map.get(target - nums[i]);
+            if (null != value) {
+                return new int[]{i, value};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
