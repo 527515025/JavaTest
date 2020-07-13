16f79baec0681f929526c50d6d67926055bf9d6e
杨伊博 <527515025@qq.com>
2020-07-13 14:51:00 +0800

    add find second min

--- src/main/java/com/us/acm/FindTreeSecondMaxValue.java
+++ src/main/java/com/us/acm/FindTreeSecondMaxValue.java
@@ -4,6 +4,7 @@ import com.us.tree.LeftRightTree.LRTree;
 import com.us.tree.LeftRightTree.LRTreeApp;
 
 /**
+ * 阿里云
  * 给定一个二叉搜索树，找出其第二大的数。
  * 对于二叉搜索树，若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值； 若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值
  *
---
+++ src/main/java/com/us/acm/FindTreeSecondMixValue.java
@@ -0,0 +1,42 @@
+package com.us.acm;
+
+import com.us.tree.LeftRightTree.LRTree;
+import com.us.tree.LeftRightTree.LRTreeApp;
+
+/**
+ * 力扣
+ * 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+ * <p>
+ * 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+ *
+ * 问题可以转化为求左右子树的最小值，如果左右子树最小值都大于根节点的值取较小的值。其他情况取左右子树较大的值。
+ *
+ * @author yyb
+ * @time 2020/7/10
+ */
+public class FindTreeSecondMixValue {
+
+    public static void main(String[] args) {
+        LRTree tree = LRTreeApp.init();
+        long second = getSecondMin(tree.getRoot(), tree.getRoot().value);
+        System.out.println(second);
+
+    }
+
+    private static long getSecondMin(LRTree.Node root, long val) {
+        if (root == null) {
+            return -1L;
+        }
+        if (root.value > val) {
+            return root.value;
+        }
+        long l = getSecondMin(root.leftChild, val);
+        long r = getSecondMin(root.rightChild, val);
+        if (l > val && r > val) {
+            return Math.min(l, r);
+        }
+        return Math.max(l, r);
+    }
+
+
+}
