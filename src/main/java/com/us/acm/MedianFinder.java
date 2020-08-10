7ad45eeb729aa448db580e89f09d0a08666b784d
杨伊博 <527515025@qq.com>
2020-08-10 18:29:56 +0800

    使用PriorityQueue 优先队列，做堆排序

--- src/main/java/com/us/acm/MedianFinder.java
+++ src/main/java/com/us/acm/MedianFinder.java
@@ -1,8 +1,7 @@
 package com.us.acm;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 /**
  * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
@@ -15,41 +14,48 @@ import java.util.stream.Collectors;
  *
  * @author yyb
  * @time 2020/5/20
+ * <p>
+ * 思路，获取流数据后进行排序，size%2 ==0 则取中间两个数相加除以 2 ，如果 size%2=1 则取中间数
+ * 由于 stream.sorted 是自然排序所以比较慢，时间不符合要求，顾采用堆排序
  */
 public class MedianFinder {
-    private List<Double> init;
-
     /**
-     * initialize your data structure here.
+     * 优先队列，PriorityQueue 线程不安全，多线程使用 PriorityBlockingQueue
+     * 该队列的头部是相对于指定顺序的最小元素
      */
+    private PriorityQueue<Integer> maxHeap, minHeap;
+
+
     public MedianFinder() {
-        init = new ArrayList<>();
+        //大顶堆 优先队列实现的大顶堆
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        //小顶堆 优先队列实现的小顶堆
+        minHeap = new PriorityQueue<>();
     }
 
     public void addNum(int num) {
-        init.add(Double.valueOf(num));
+        //将指定的元素插入到此优先级队列中。
+        maxHeap.offer(num);
+        //将大顶堆中最小的元素放到小顶堆
+        minHeap.offer(maxHeap.poll());
+        //如果小顶堆中数大于
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
     }
 
     public double findMedian() {
-        if (init == null) {
-            return -1;
+        if (maxHeap.size() == minHeap.size()) {
+            //peek检索但不删除此队列的头，如果此队列为空，则返回 null 。
+            return (maxHeap.peek() + minHeap.peek()) * 0.5;
         }
-        List<Double> sort = init.stream().sorted().collect(Collectors.toList());
-        int remainder = sort.size() % 2;
-        int merchant = sort.size() / 2;
-        double median;
-        if (remainder == 1) {
-            median = sort.get(merchant);
-        } else {
-            median = (sort.get(merchant - 1) + sort.get(merchant)) / 2;
-        }
-        return median;
+        return maxHeap.peek();
     }
 
 
     public static void main(String[] args) {
         MedianFinder finder = new MedianFinder();
-        finder.addNum(1);
+        finder.addNum(11);
         finder.addNum(7);
         finder.addNum(15);
         finder.addNum(116);
@@ -58,9 +64,6 @@ public class MedianFinder {
         System.out.println(finder.findMedian());
 
     }
-
-
-
 
 
 }
