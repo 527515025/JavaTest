afd0295386dbf3adbee6f49de61e3e2f89e31ff3
杨伊博 <527515025@qq.com>
2020-10-12 19:09:12 +0800

    提交官方解答

--- src/main/java/com/us/acm/SumTest.java
+++ src/main/java/com/us/acm/SumTest.java
@@ -17,7 +17,7 @@ public class SumTest {
     public static void main(String[] args) {
 //        int[] result = twoSum(new int[]{3, 2, 4}, 6);
 //        Arrays.stream(result).parallel().forEach(x -> System.out.println(x));
-        threeSum(new int[]{-1,0,1,2,-1,-4});
+        threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
     /**
@@ -78,6 +78,8 @@ public class SumTest {
      * 思路：
      * 排序+双指针法
      *
+     * 	1429 ms	45.1 MB
+     *
      * @param nums
      * @return
      */
@@ -112,5 +114,55 @@ public class SumTest {
         }
         return result;
     }
+
+
+    /**
+     * 官方题解 双指针
+     *
+     * 23 ms	43 MB
+     *
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSumL(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
 
 }
