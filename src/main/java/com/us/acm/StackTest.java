dd563d3b54d5bc124539f2bc3458a22987e82ea2
杨伊博 <527515025@qq.com>
2020-10-27 17:28:37 +0800

    add 最小栈

--- src/main/java/com/us/acm/StackTest.java
+++ src/main/java/com/us/acm/StackTest.java
@@ -12,8 +12,9 @@ import java.util.Stack;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
+ * <p>
  * 思路：两个栈的反转为 队列
+ *
  * @author yyb
  * @time 2020/10/27
  */
@@ -40,5 +41,62 @@ public class StackTest {
             }
         }
         return output.isEmpty() ? -1 : output.pop();
+    }
+
+
+    /**
+     * 最小栈
+     * <p>
+     * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。时间复杂度 o1
+     * <p>
+     * push(x) —— 将元素 x 推入栈中。
+     * pop() —— 删除栈顶的元素。
+     * top() —— 获取栈顶元素。
+     * getMin() —— 检索栈中的最小元素。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/min-stack
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * <p>
+     * <p>
+     * 	6 ms 39.8 MB
+     */
+    class MinStack {
+        Stack<Integer> stack;
+        private Integer min;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack() {
+            stack = new Stack<>();
+            min = Integer.MAX_VALUE;
+        }
+
+        public void push(int x) {
+            if (min >= x) {
+                //先把当前最小值入栈记录 成为第二小的值
+                stack.push(min);
+                min = x;
+
+            }
+            stack.push(x);
+        }
+
+        public void pop() {
+            Integer x = stack.pop();
+            if (x.equals(min)) {
+                min = stack.pop();
+            }
+        }
+
+        public int top() {
+            //获取栈顶元素，但是不删除
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return min;
+        }
     }
 }
