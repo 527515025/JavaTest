9fa95de6c396a5765ac35b1795bfaf3f702d5a1b
杨伊博 <527515025@qq.com>
2020-11-06 10:33:32 +0800

    反转字符串

--- src/main/java/com/us/acm/StringTest.java
+++ src/main/java/com/us/acm/StringTest.java
@@ -14,9 +14,11 @@ public class StringTest {
 
     public static void main(String[] args) {
 
-        System.out.println(patternMatching("( )"));
+//        System.out.println(patternMatching("( )"));
 //        System.out.println(maxFreq("abbabbabbceabbede", 3, 2, 4));
 //        System.out.println("findRepeat: " + findRepeat(init()));
+        reverseString(new char[]{});
+
     }
 
 
@@ -168,6 +170,32 @@ public class StringTest {
         return stack.isEmpty();
     }
 
+
+    /**
+     * 反转字符串
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * <p>
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * <p>
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reverse-string
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param s
+     */
+    public static void reverseString(char[] s) {
+        if (s.length == 0) {
+            return;
+        }
+        int l = s.length - 1;
+        for (int i = 0; i <= l / 2; i++) {
+            char tmp = s[i];
+            s[i] = s[l - i];
+            s[l - i] = tmp;
+        }
+    }
 
 }
 
