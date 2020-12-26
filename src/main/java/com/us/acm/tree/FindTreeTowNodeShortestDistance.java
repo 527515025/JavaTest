7d0b3aa4175dc781262b953802fca5b61334f1a9
杨伊博 <527515025@qq.com>
2020-12-26 17:52:22 +0800

    二叉树的任意两个节点的距离

--- src/main/java/com/us/acm/tree/FindTreeTowNodeShortestDistance.java
+++ src/main/java/com/us/acm/tree/FindTreeTowNodeShortestDistance.java
@@ -4,8 +4,9 @@ import java.util.LinkedList;
 
 /**
  * 求一个二叉树的任意两个节点的距离
+ * 距离是指连接两个节点所需要的最小边的条数。
  * https://blog.csdn.net/qq_38251888/article/details/88831919
- *
+ * <p>
  * java
  * https://zhuanlan.zhihu.com/p/82754383
  * <p>
@@ -29,22 +30,35 @@ public class FindTreeTowNodeShortestDistance {
 
     /**
      * 返回两个结点之间的距离通过公共父节点
-     * 首先需要找到两个结点的公共祖先的位置，设公共祖先的位置是fatherNode.
-     * 则 distence(nodeOne,nodeTwo) = distence(root,nodeOne) + distence(root,nodeTwo) - 2 * distence(root,fatherNode);
+     * 首先需要找到两个结点的 公共祖先 的位置，设公共祖先的位置是 fatherNode.
+     * 则 distance(nodeOne,nodeTwo) = distance(root,nodeOne) + distance(root,nodeTwo) - (2 * distance(root,fatherNode));
+     * 即 根节点到两节点距离的和 减去 两个根节点到最近父节点到距离
+     * <p>
+     * <p>
      * 所以第一步需要找到两个结点的公共父节点
-     * 实现distence函数，所谓的distence函数就是在计算结点所在的层数。
+     * 实现distance函数，所谓的distance函数就是在计算结点所在的层数。
      *
      * @param root
      * @param nodeOne
      * @param nodeTwo
      * @return
      */
-    public static int getTwoTreeNodeDistence(TreeNode root, TreeNode nodeOne, TreeNode nodeTwo) {
+    public static int getTwoTreeNodeDistance(TreeNode root, TreeNode nodeOne, TreeNode nodeTwo) {
         TreeNode fatherNode = findCommonFatherNode(root, nodeOne, nodeTwo);
-        int x = distance(root, nodeOne) + distance(root, nodeTwo) - 2 * distance(root, fatherNode);
+        int one = distance(root, nodeOne);
+        int two = distance(root, nodeTwo);
+        int fa = distance(root, fatherNode);
+        int x = one + two - 2 * fa;
         return x;
     }
 
+    /**
+     * 寻找距离即，广度优先遍历，找到该节点锁在层数，返回 层深 -1 即为距离 边的个数。
+     *
+     * @param root
+     * @param node
+     * @return
+     */
     private static int distance(TreeNode root, TreeNode node) {
         // 找到node所在的层数
         int level = 0;
@@ -56,7 +70,7 @@ public class FindTreeTowNodeShortestDistance {
             for (int i = 0; i < size; i++) {
                 TreeNode tempNode = queue.removeFirst();
                 if (tempNode == node) {
-                    return level;
+                    return level - 1;
                 }
                 if (tempNode.left != null) {
                     queue.addLast(tempNode.left);
@@ -69,6 +83,15 @@ public class FindTreeTowNodeShortestDistance {
         return -1;
     }
 
+    /**
+     * 寻找两个节点的父节点
+     * 左右子树中找到该节点后，最终会汇合到一个父节点，返回父节点
+     *
+     * @param root
+     * @param nodeOne
+     * @param nodeTwo
+     * @return
+     */
     private static TreeNode findCommonFatherNode(TreeNode root, TreeNode nodeOne, TreeNode nodeTwo) {
         // 递归寻找公共父节点
         if (root == null || root == nodeOne || root == nodeTwo) {
@@ -83,12 +106,14 @@ public class FindTreeTowNodeShortestDistance {
             return null;
         }
         if (left == null) {
-            // 左边没找到，右边找到了，说明，right就是公共祖先
+            // 左边没找到，右边找到了，说明，right就是公共祖先，两个节都在left
             return right;
         }
         if (right == null) {
+            // 右边没找到，左边找到了，说明，left就是公共祖先，两个节点都在left
             return left;
         }
+        // 最终汇合节点，两个节点分布左右子树，则该节点为父节点
         return root;
     }
 
@@ -103,10 +128,14 @@ public class FindTreeTowNodeShortestDistance {
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(7);
         root.right.left.right = new TreeNode(8);
-        System.out.println("Dist(8,7) = " + getTwoTreeNodeDistence(root, root.right.left.right, root.right.right));
 
-        System.out.println("Dist(8,3) = " + getTwoTreeNodeDistence(root, root.right.left.right, root.right));
+        System.out.println("3 Dist(8,7) = " + getTwoTreeNodeDistance(root, root.right.left.right, root.right.right));
 
-        System.out.println("Dist(8,2) = " + getTwoTreeNodeDistence(root, root.right.left.right, root.left));
+        System.out.println("2 Dist(8,3) = " + getTwoTreeNodeDistance(root, root.right.left.right, root.right));
+
+        System.out.println("4 Dist(8,2) = " + getTwoTreeNodeDistance(root, root.right.left.right, root.left));
+
+        System.out.println("0 Dist(8,8) = " + getTwoTreeNodeDistance(root, root.right.left.right, root.right.left.right));
+
     }
 }
